03d3b6311a9beeb0017fb7f62e4b3ab28696a713
julieta mores <julieta.mores@itscordoba.edu.ar>
2023-01-29 22:11:53 -0300

    subida con nuevos errores

--- applications/Cliente/admin.py
+++ applications/Cliente/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from .models import Cliente 
 
-admin.site.register(Cliente, ClienteAdmin )
 
-class ClienteAdmin (admin.ModelAdmin): 
-    list_display = (
-        'apellido',
-        'nombre',
-        'razonSocial',
-        'documento',
-    )
 
-    search_fields = ('apellidos',)
-    list_filter = ('razonSocial',)
+admin.site.register(Cliente,)
